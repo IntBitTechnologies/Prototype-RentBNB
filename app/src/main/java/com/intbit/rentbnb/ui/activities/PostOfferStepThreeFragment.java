d52dc54eb788689186ba015f669c20cf5b836274
AdibaTajAtIntBitTech <adiba.taj@intbittech.com>
2016-11-17 19:38:14 +0530

    fragments

--- app/src/main/java/com/intbit/rentbnb/ui/activities/PostOfferStepThreeFragment.java
+++ app/src/main/java/com/intbit/rentbnb/ui/activities/PostOfferStepThreeFragment.java
@@ -1,11 +1,16 @@
 package com.intbit.rentbnb.ui.activities;
 
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.intbit.rentbnb.R;
 import com.intbit.rentbnb.base.RentbnbBaseFragment;
@@ -15,24 +20,49 @@ import com.intbit.rentbnb.base.RentbnbBaseFragment;
  */
 
 public class PostOfferStepThreeFragment extends RentbnbBaseFragment {
-    Button step4;
+    private Button step3NextButton;
+    private SeekBar conditionSeekBar;
+    private TextView conditionTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.tab_post_offer_step_3, container, false);
-        step4 = (Button) v.findViewById(R.id.step4);
+        conditionSeekBar = (SeekBar) v.findViewById(R.id.tab_post_offer_step_3_condition_seekBar);
+        conditionTextView = (TextView) v.findViewById(R.id.tab_post_offer_step_3_condition_tracker_textView);
+        step3NextButton = (Button) v.findViewById(R.id.tab_post_offer_step_3_next_button);
 
-        step4.setOnClickListener(new View.OnClickListener() {
+        step3NextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //TODO Do all Validations and Save Data
 
                 //Trigger code for changing page
                 Preferences.setCurrentPage(4);
-                ((PostOfferActivity)getActivity()).changefragmentalongStepProcess(4);
+                ((PostOfferActivity) getActivity()).changefragmentalongStepProcess(4);
             }
         });
+
+        // Initialize the textview with '0'.
+        conditionTextView.setText("Condition: " + conditionSeekBar.getProgress() + "/" + conditionSeekBar.getMax());
+        conditionSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 5;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                conditionTextView.setText("Condition: " + progress + "/" + seekBar.getMax());
+            }
+        });
+
         return v;
     }
 }
